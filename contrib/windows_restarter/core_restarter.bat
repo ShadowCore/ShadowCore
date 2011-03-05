922b56c53b1aa4ce4059eef5a9dfdcf85c458d44
ShadowCore <shaddowcore@gmail.com>
2011-03-04 17:10:43 -0800

    put into a folder to make foom for futer diffrent patch cores

--- contrib/windows_restarter/core_restarter.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@title OregonCore
-CLS
-ECHO Initializing Core (World-Server)...
-:1
-start "Oregon Core" /B /MIN /WAIT oregon-core.exe -c oregoncore.conf
-if %errorlevel% == 0 goto end
-goto 1
-:end
--- contrib/windows_restarter/realm_restarter.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@title OregonRealm
-CLS
-ECHO Initializing Realm (Logon-Server)...
-:1
-start "Oregon Realm" /B /MIN /WAIT oregon-realm.exe -c oregonrealm.conf
-if %errorlevel% == 0 goto end
-goto 1
-:end
